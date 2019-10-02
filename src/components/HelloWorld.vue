20622a413ffbdc17a8a82ff4514b75c267163e7f
Joshua Bennett <joshuabennett@utexas.edu>
2019-10-02 00:36:53 -0500

    More structure and styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
+  <div class='home'>
+    <div class="title is-size-3 has-text-centered">Reddit Search</div>
+    <div class="control searchbar has-icons-left has-icons-right">
+      <input class="input is-small" type="email" placeholder="Search Reddit">
+      <span class="icon is-small is-left">
+        <i class="fas fa-envelope"></i>
+      </span>
+      <span class="icon is-small is-right">
+        <i class="fas fa-search"></i>
+      </span>
+    </div>
     <search-result></search-result>
     <search-result></search-result>
     <search-result></search-result>
@@ -25,6 +35,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.searchbar {
+  width: 30vw;
+  margin: 0 auto;
+  margin-bottom: 2em;
+}
 h1, h2 {
   font-weight: normal;
 }
--- src/components/searchResult.vue
+++ src/components/searchResult.vue
@@ -3,6 +3,22 @@
         <div class="post-title">Reddit Post Title</div>
         <hr>
         <div class="post-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis inventore magni aspernatur natus expedita culpa qui, exercitationem doloremque nemo optio asperiores sapiente consectetur sunt commodi in delectus velit alias praesentium!</div>
+        <hr>
+        <div class="comment-container">
+            <div class="upvotes">100</div>
+            <div class="post-top-comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit aut unde blanditiis laudantium ullam facere, nulla reiciendis. Voluptatem recusandae facere consequuntur? Ad facilis iure doloribus in minima eius sit magni!</div>
+        </div>
+        <div class="comment-container">
+            <div class="upvotes">100</div>
+            <div class="post-top-comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit aut unde blanditiis laudantium ullam facere, nulla reiciendis. Voluptatem recusandae facere consequuntur? Ad facilis iure doloribus in minima eius sit magni!</div>
+        </div>
+    
+        <nav class="breadcrumb is-right" aria-label="breadcrumbs">
+        <ul>
+            <li><a href="#">Expand Comments</a></li>
+            <li class="is-active"><a href="#" aria-current="page">View Thread</a></li>
+        </ul>
+        </nav>
     </div>
 </template>
 
@@ -13,16 +29,33 @@ export default {
 </script>
 
 <style>
+.comment-container {
+    display: flex;
+    flex-direction: row;
+    margin-top: 2vh;
+    justify-content: center;
+    align-items: center;
+}
+.upvotes {
+    padding: 1em;
+    color: green;
+    background-color: rgba(0, 128, 0, 0.096);
+    margin: 1em;
+
+}
+.breadcrumb {
+    margin: 1em;
+}
 .container {
     min-height: 20vh;
     width: 90vw;
-    border: 1px dotted black;
-    box-shadow: 0px 4px 8px gray;
+    border: 1px dotted rgba(0, 0, 0, 0.151);
+    box-shadow: 0px 4px 8px rgba(128, 128, 128, 0.589);
 }
 hr {
     margin: 0;
     padding: 0;
-    background-color: black;
+    background-color: rgba(0, 0, 0, 0.205);
     border: none;
     height: 1px;
     margin-top: 10px;
@@ -31,5 +64,10 @@ hr {
 .post-title {
     font-weight: bold;
     font-size: 18px;
+}
+.post-top-comment {
+    padding: 1em;
+    background-color: rgba(211, 211, 211, 0.507);
+
 }
 </style>
