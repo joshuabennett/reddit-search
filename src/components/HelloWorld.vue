d44c3ba8c160d7bc245bf770cf6870f3e0fc430a
Joshua Bennett <joshuabennett@utexas.edu>
2019-10-01 23:47:12 -0500

    Initial commit with project structure.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <search-result></search-result>
+    <search-result></search-result>
+    <search-result></search-result>
+  </div>
+</template>
+
+<script>
+import searchResult from './searchResult.vue'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    'search-result': searchResult
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/searchResult.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container box is-primary">
+        <div class="post-title">Reddit Post Title</div>
+        <hr>
+        <div class="post-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis inventore magni aspernatur natus expedita culpa qui, exercitationem doloremque nemo optio asperiores sapiente consectetur sunt commodi in delectus velit alias praesentium!</div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container {
+    min-height: 20vh;
+    width: 90vw;
+    border: 1px dotted black;
+    box-shadow: 0px 4px 8px gray;
+}
+hr {
+    margin: 0;
+    padding: 0;
+    background-color: black;
+    border: none;
+    height: 1px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+.post-title {
+    font-weight: bold;
+    font-size: 18px;
+}
+</style>
